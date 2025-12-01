387c78586ea87bb6b02b8a691e1d55be4e95974d
hody-chang <107836041+hody-chang@users.noreply.github.com>
2025-12-01 11:37:40 +0000

    Deploying to gh-pages from @ hody-chang/hody-chang.github.io@2c0338b184b5e2f7e4b328adb84da0f96fe7d3f6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "hody-chang/hodychang.github.io",
+    "data-repo": "hody-chang/hody-chang.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
